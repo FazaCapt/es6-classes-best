b06b0ff49cc7e843bfdbecb275aa912087fc1385
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-06-08 13:32:34 +0700

    Extending and Rendering

---
+++ app/components/ComponentOne.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+var ComponentOne = React.createClass({
+    render: function(){
+        return(
+            <div>
+                <h3>Component One using React.createClass</h3>
+            </div>
+        )
+    }
+})
+
+
+export default ComponentOne;
